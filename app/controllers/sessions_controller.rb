616e677b778a2a45904f11b970e61babd688c755
kateconnaughton <kate.connaughton@kellogg.northwestern.edu>
2023-03-06 14:45:36 +0000

    take2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,27 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by({ "email" => params["email"] })
+    @user = User.find_by({ "email" => params["email"]})
     if @user
       if BCrypt::Password.new(@user["password"]) == params["password"]
-        session["user_id"] = @user["id"]
-        flash["notice"] = "Welcome, #{@user["username"]}."
         redirect_to "/posts"
       else
-        flash["notice"] = "Nope."
-        redirect_to "/login"
+        redirect_to "/sessions/new"
       end
     else
-      flash["notice"] = "Nope."
-      redirect_to "/login"
+      redirect_to "/sessions/new"
     end
   end
-  end
-
-  def destroy
-    flash["notice"] = "Goodbye."
-    session["user_id"] = nil
-    redirect_to "/login"
-  end
 end
-  
+        
+     
