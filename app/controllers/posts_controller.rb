e796f4db231fe9b81594dc695cb1786ff73c2889
kateconnaughton <kate.connaughton@kellogg.northwestern.edu>
2023-03-07 20:15:50 +0000

    take7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,21 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all 
+  end
+
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
   end
 
   def create
+    if @current_user
     @post = Post.new
+    @post["user_id"] = @current_user["id"]
     @post["title"] = params["post"]["title"]
+    @post["image"] = params["post"]["image"]
+    @post.uploaded_image.attach (params["post"]["uploaded_image"])
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
