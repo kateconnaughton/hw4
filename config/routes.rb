a85dd73dd6156695050213d0d486a932057db41d
kateconnaughton <kate.connaughton@kellogg.northwestern.edu>
2023-03-07 04:04:12 +0000

    take3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get "/", :controller => "places", :action => "index"
-  get "/login", :controller => "sessions", :action => "new"
-  get "/logout", :controller => "sessions", :action => "destroy"
+  get "/", :controller => "posts", :action => "index"
   resources "places"
   resources "posts"
   resources "sessions"
   resources "users"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
 end
